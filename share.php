df2cbf7c1c946d51e3e45877127a8c88b0975834
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-06-04 12:02:09 +0300

    Add files via upload

---
+++ share.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Transgood</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="assets\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
+
+</head>
+<body  style="background:black;">
+	<div class="container">
+	   <div class="navbar navbar-expand-lg navbar-light bg-success">
+
+	     <div class="navbar-collapse">
+		  <ul class="navbar-nav mr-auto">
+		  	<li class="nav-item active">
+          	<div class="jumbotron" style="background-image: url(image/logo.jpg);background-size: cover;">
+                <h1 style="color:green">Trans<em style="color:blue">G</em></h1>
+                </div>
+                </li>
+
+                <li class="nav-item">
+                <div class="jumbotron bg-success"> 
+              <h1><em>Tries the chance to enjoy the journey</em></h1>
+          </div>
+          </li>
+      </ul>
+	
+
+      </div>
+   </div>
+ </div><br>
+
+
+<div class="container bg-success">
+   <div class="row">
+     <div class="col col-sm 1">
+      
+     </div>
+     <div class="col-sm 5">
+      <center>
+       <h1 style="font-size: 30px;color: white">SHARE &nbsp;</h1>
+       </center>
+     
+     </div>
+     <div class="col-sm 1">
+       <center><li class="fa fa-share-alt fa-3x" style="color:white"></li></center>
+     </div>
+
+     <button class="btn bg-success" style="font-size:30px;color: white">X</button>
+</div>
+</div><br>
+
+ <div class="container">
+   <div class="row">
+     <div class="col col-sm 1">
+       
+     </div>
+     <div class="col col-sm 5 bg-white"><br><br>
+      
+        <center>
+         <button class="btn btn-outline-success"><a href="https://www.facebook.com" target="new"> <i class="fab fa-facebook-f fa-4x" style="color: blue;width: 90px"></i></a><br>
+          Facebook
+          </button> 
+        </center>
+    <br><br>
+      
+          <center>
+        <button class="btn btn-outline-success"><a href="https://www.gmail.com" target="new">  <i class="fab fa-google-plus fa-4x" style="color:red;width: 90px"></i></a><br>
+          Gmail
+        </button> 
+          </center>
+     
+         <br><br>
+          <center>
+          <button class="btn btn-outline-success"><a href="https://www.youtube.com" target="new"> <i class="fab fa-youtube fa-4x" style="color: red;width: 90px"></i></a><br>
+            Youtube
+            </button> 
+           </center>
+       <br><br>
+
+     </div>
+     <div class="col col-sm 5 bg-white"><br> <br> 
+       
+        <center>
+          <button class="btn btn-outline-success"><a href="https://www.instagram.com" target="new"><i class="fab fa-instagram fa-4x" style="color:black;width: 90px"></i></a><br>
+            Instagram
+            </button> 
+           </center>
+         <br><br>
+      
+      
+        <center>
+          <button class="btn btn-outline-success"><a href="https://www.twitter.com" target="new"> <i class="fab fa-twitter fa-4x" style="color: blue;width: 90px"></i></a><br>
+            Twitter
+          </button> 
+            </center>
+       <br><br>
+        
+          <center>
+           <button class="btn btn-outline-success"><a href="https://www.whatsapp.com" target="new"> <i class="fab fa-whatsapp fa-3x" style="color: green;width: 90px"></i></a><br>
+            WhatsApp
+            </button> 
+            </center>
+            <br><br>
+       
+     
+     </div>
+     <div class="col col-sm 1">
+    
+     </div>
+   </div>
+ </div>
+
+ <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/popper.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
