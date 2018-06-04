54727794209b5627dc3ea046afde0980f4ddaef1
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-06-04 11:48:17 +0300

    Add files via upload

---
+++ infoedit.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Transgood</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="assets\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
+</head>
+<body  style="background:black;">
+	<div class="container">
+	   <div class="navbar navbar-expand-lg navbar-light bg-success">
+
+	     <div class="navbar-collapse">
+		  <ul class="navbar-nav mr-auto">
+		  	<li class="nav-item active">
+          	<div class="jumbotron" style="background-image: url(image/logo.jpg);background-size: cover;">
+                <h1 style="color:green">Trans<em style="color:blue">G</em></h1>
+                </div>
+                </li>
+
+                <li class="nav-item">
+                <div class="jumbotron bg-success"> 
+              <h1><em>Tries the chance to enjoy the journey</em></h1>
+          </div>
+          </li>
+      </ul>
+	
+
+      </div>
+   </div>
+ </div><br>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-2">
+			
+		</div>
+		<div class="col-sm-8">
+			<h2>Are you shure you want to edit your profile</h2>
+			<form class="form-horizontal" action="profile.php" method="POST">
+				<input class="form-control" type="text" name="fname" placeholder="Previous first name" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="New" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="Previous last name" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="New" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="Previous" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="New" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="Previous" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="New" style="font-size: 18px"><br>
+				<input class="form-control" type="text" name="fname" placeholder="Previous" style="font-size: 18px"><br>
+			</form>
+		</div>
+		<div class="col-sm-2">
+			
+		</div>
+
+	`</div>
+</div>
+
+
+  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/popper.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
