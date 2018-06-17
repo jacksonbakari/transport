9461f361f7ed8f15efcc45f7b4b36e07fe716007
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-06-18 01:17:19 +0300

    Add files via upload

---
+++ commentsite.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Transgood</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" src="assets/bootstrap/js/popover.min.js">
+    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script> 
+     <script src="assets/bootstrap/js/jquery.min.js"></script> 
+  <link rel="stylesheet" type="text/css" src="assets\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
+
+  <style>    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid bg-dark">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar">o</span>
+        <span class="icon-bar">o</span>
+        <span class="icon-bar">o</span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Messages</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group input-group">
+          <input type="text" class="form-control" placeholder="Search..">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>        
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container text-center">    
+  <div class="row">
+    <div class="col-sm-3 well" style="background:lightgray">
+      <div class="well bg-dark">
+        <p><a href="#">My Profile</a></p>
+        <img src="image/face.png" class="img-circle" height="65" width="65" alt="Avatar">
+      </div>
+      <div class="well bg-success">
+        <p><a href="#">Interests</a></p>
+        <p>
+          <span class="label label-default" style="color:blue;font-weight:bold">News</span>
+          <span class="label label-primary" style="color:blue;font-weight:bold">W3Schools</span>
+          <span class="label label-success" style="color:blue;font-weight:bold">Labels</span>
+          <span class="label label-info" style="color:blue;font-weight:bold">Football</span>
+          <span class="label label-warning" style="color:blue;font-weight:bold">Gaming</span>
+          <span class="label label-danger" style="color:blue;font-weight:bold">Friends</span>
+        </p>
+      </div>
+      <div class="alert alert-success fade in">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+        <p><strong>Ey!</strong></p>
+        People are looking at your profile. Find out who.
+      </div>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-7">
+    
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="panel panel-default text-left">
+            <div class="panel-body">
+              <p contenteditable="true">Status: Feeling Blue</p>
+              <button type="button" class="btn btn-default btn-sm">
+                <span class="glyphicon glyphicon-thumbs-up"></span> Like
+              </button>     
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+      <div class="row" style="border:2px solid Violet;">
+        <div class="col-sm-3 border-right">
+          <div class="well" >
+           <p>John</p>
+           <img src="image/hores.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+          </div>
+        </div>
+      </div>
+<br>
+      <div class="row" style="border:2px solid Violet;">
+        <div class="col-sm-3">
+          <div class="well">
+           <p>Bo</p>
+           <img src="image/hores.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+          </div>
+        </div>
+        <div class="col-sm-9" >
+          <div class="well">
+            <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+          </div>
+        </div>
+      </div>
+<br>
+      <div class="row" style="border:2px solid Violet;">
+        <div class="col-sm-3">
+          <div class="well">
+           <p>Jane</p>
+           <img src="image/hores.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+          </div>
+        </div>
+      </div>
+<br>
+      <div class="row" style="border:2px solid Violet;">
+        <div class="col-sm-3">
+          <div class="well">
+           <p>Anja</p>
+           <img src="image/hores.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div class="well">
+            <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+          </div>
+        </div>
+      </div>   
+<br>
+    </div>
+    <div class="col-sm-2 well">
+      <div class="thumbnail">
+        <p>Upcoming Events:</p>
+        <img src="image/hores.jpg" alt="Paris" width="400" height="300">
+        <p><strong>Paris</strong></p>
+        <p>Fri. 27 November 2015</p>
+        <button class="btn btn-primary">Info</button>
+      </div>      
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+ <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/popper.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+<!-- Mirrored from www.w3schools.com/bootstrap/tryit.asp?filename=trybs_temp_social&stacked=h by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 30 Mar 2018 13:09:08 GMT -->
+</html>
