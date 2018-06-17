8495c69ee7ea0566128c34b2eac8607b3ecc223a
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-06-18 01:18:27 +0300

    Add files via upload

---
+++ goforum.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POCKTClass.com</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body class="container">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#" style="color:green; font-size: 55px;">POCK<em style="color:blue;font-size: 68px; ">C</em>lass</a>
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"style="color: blue;">SCHooL<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: blue;">GoForum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" style="color: blue;">Contents</a>
+      </li>
+    </ul>
+
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm type="submit">Search</button>
+    </form>
+
+
+    
+
+  </div>
+</body>
+  </html>
