9100e1bf7d025bc02cf3840e11126b839f4c3da5
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-06-18 13:54:16 +0300

    Add files via upload

---
+++ nel.php
@@ -0,0 +1,364 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   <title>DARUSO COICT</title>
+  
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stylesheet.css" rel="stylesheet">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="assets\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
+</head>
+<body>
+
+
+  </head>
+
+  <body>
+
+   <!--................................. nav bar start here ...........................................-->
+
+    <div class="navbar navbar-expand-md fixed-top bg-primary">
+      
+      <a class="navbar-brand" href="Home.html" style="color: white" style="color: white">DARUSO</a>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="home.html" style="color: white">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="categories.html" style="color: white">categories</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="CollegeGazette.html" style="color: white">CollegeGazette</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white">Leaders</a>
+            <div class="dropdown-menu bg-dark" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#" style="color: white">Action</a>
+              <a class="dropdown-item" href="#" style="color: white">Another action</a>
+              <a class="dropdown-item" href="#" style="color: white">Something else here</a>
+            </div>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white">Staffs</a>
+            <div class="dropdown-menu bg-dark" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#" style="color: white">Action</a>
+              <a class="dropdown-item" href="#" style="color: white">Another action</a>
+              <a class="dropdown-item" href="#" style="color: white">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-right">
+          <li class="nav-item active">
+            <a class="nav-link" href="Profile.html" style="color: white">WELCOME<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+           <a class="nav-link" data-toggle="modal" href="#" data-target="#log" style="color: white"><span class="sr-only">(current)</span>LogIn</a>
+            
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+    </div>
+   <!-- ......................................End of nav bar ........................................-->
+
+<br><br><br><br>
+
+<!--......................................... HEADER START HERE............................. -->
+ <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-1">
+
+          </div>
+          <div class="col col-md-10">
+             <center><h1 style="border:2px solid Violet; color:blue;"><span><img src="Icon.jpg" alt="" height="100px"width="100px" style="color: white"></span>DARUSONewspaper
+            </br>  <small>CoICT College of Information and Technology</small> </h1></center>
+          </div>
+
+          <div class="col-md-1">
+
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+<!--............................................ END OF HEDER................................ -->
+
+
+
+ <!-- ..............................................DIV 001 ...................................-->
+  <div class="container">
+       <div class="row">
+        <div class=" col col-md-8 images">
+
+         <!--  <img src="PageWallpaper.jpg" alt="Daruso" height="500px" width="1200px"> -->
+
+        <h3 id="txt">DarusoNewsPlatform</h3>
+         <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-danger" onclick="document.getElementById('demo').innerHTML = Date()" data-toggle="modal" data-target="#myModal">
+      Trending NEWS</button>
+
+<!-- The Modal -->
+
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+
+      <div class="modal-header">
+        <h4 class="modal-title">Popular Headings</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+
+      <div class="modal-body">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+       <button class="btn btn-outline-primary" style="float:right;">More...</button>
+      </div>
+
+      <!-- Modal footer -->
+
+      <div class="modal-footer">
+        <p id="demo" style="color:black; background:green;"></p>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div><br>
+</div>
+ </div>
+</div>
+
+<!-- ...............................................</DIV>..................................... -->
+
+<br><br>
+
+<!-- ..............................................IMAGE SECITONS ................................-->
+ <div class="container">
+        <div class="row">
+         <div class="col-md-3">
+          <div class="card" style="width: auto;">
+            <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>            </div>
+          </div>
+         </div>
+      
+           <div class="col-md-3">
+             <div class="card" style="width: auto;">
+               <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+               <div class="card-body">
+                 <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>
+                </div>
+              </div>
+            </div>
+           <div class="col-md-3">
+            <div class="card" style="width: auto;">
+              <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>
+              </div>
+            </div>
+          </div><div class="col-md-3">
+            <div class="card" style="width: auto;">
+              <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>
+              </div>
+            </div>
+          </div></div><br>
+          <div class="container">
+        <div class="row news">
+            <div class="col-md-3">
+              <div class="card" style="width: auto;">
+                <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>
+                </div>
+              </div>
+            </div>
+          
+              <div class="col-md-3">
+                <div class="card" style="width: auto;">
+                  <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card" style="width: auto;">
+                  <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p>                  </div>
+                </div>
+              </div><div class="col-md-3">
+                <div class="card" style="width: auto;">
+                  <img class="card-img-top" src="image/beu1.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text"><a href="Abc.html">Some quick example text to build on the card title and make up the bulk of the card's content.</a></p> 
+                  </div>
+                </div>
+              </div>
+            </div>
+<!--................................................. END IMAGE/................................... -->
+
+<br><br>
+
+ <!--...............................................NAVIGATORRR..................................  -->
+  <div class="container">
+     <nav aria-label="Page navigation example" style="float:right;">
+        <ul class="pagination align-bottom">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+   </div>
+ <!-- ..............................................END NAVIGATOR................................... -->
+    
+
+<!-- ..............................................FOOTERR........................................... -->
+<div class="container bg-dark">
+<footer>
+    <div class="row" style="color:white">
+      <div class="col col-md-5"></div>
+      <div class="col col-md-4">
+        <p>&copy Copy right 2108 DARUSO @ Inc.</p>
+     <a href="https://www.instagram.com" target="new"><i class="fab fa-instagram fa-2x" style="color:black"></i></a>
+        <a href="https://www.gmail.com" target="new">  <i class="fab fa-google-plus fa-2x" style="color:red"></i></a>
+        <a href="https://www.facebook.com" target="new"> <i class="fab fa-facebook-f fa-2x" style="color: blue"></i></a>
+        <a href="https://www.twitter.com" target="new"> <i class="fab fa-twitter fa-2x" style="color: blue"></i></a>
+        <a href="https://www.whatsapp.com" target="new"> <i class="fab fa-whatsapp fa-2x" style="color: green"></i></a>
+          <a href="https://www.youtube.com" target="new"> <i class="fab fa-youtube fa-2x" style="color: red"></i></a>
+      </div>
+        <div class="col col-md-4"></div>
+    </div>
+</footer>
+</div>
+<!-- ...............................................//FOTERR............................................. -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <div id="log" class="modal fade"  role="dialog" style="display:none;">
+    <div class="modal-dialog modal-lg" role="document"  style="width:1000px !important;">
+    
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+         <header  style="text-align:center; font-size:20px">LOG IN</header>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+        <div class="modal-body">
+      <div class="container">
+    <form class="form" action="include/signup.php " method="POST" >
+    <div class="container">
+      <div class="row justify-content">
+        <div class="col-md-8">
+          <div class="card-group"  style="width:700px">
+            <div class="card p-4">
+              <div class="card-body" >
+                <h1>Login</h1>
+                <p class="text-muted">Sign In to your account:</p>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="icon-user"></i>
+                    </span>
+                  </div>
+                  <input type="text" name="name" class="form-control" placeholder="Username:" required="" pattern="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b">
+                </div>
+                <div class="input-group mb-4">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="icon-lock"></i>
+                    </span>
+                  </div>
+                  <input type="password" name="pass" class="form-control" placeholder="Password:" required="">
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <button type="submit" name="log" class="btn btn-dark  px-4">Login</button>
+                    <button class="btn btn-dark  px-4" style="float:right;">Exit</button>
+                  </div><br>
+                  <div class="row">
+                  <div class="col-12 text-right">
+                   <center> <button type="button" class="btn btn-link px-2">Forgot password?</button></center>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+            <div class="card text-white bg-dark py-5 d-md-down-none" style="width:44%">
+              <div class="card-body text-center">
+                <div>
+                  <h2>Sign up</h2>
+                  <p>Your advice to make sure that you have an account to this special site
+                  before using .</p>
+                  <p><a href="welcome.php" target="new" style="color:tomato">More information</a></p>
+                  <a href="reg.php" target="new1"><button type="button" class="btn btn-outline-light btn-lg" style="border:2px solid orange">Register Now!</button></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</form><br><br>
+        </div>
+        </div>
+      <!-- Modal footer -->
+      <div class="modal-footer">
+
+         <p id="demo" style="color:black; background:green;"></p>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+        <script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+});
+</script>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+ <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/popper.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+
+  </body>
+  
+</html>
